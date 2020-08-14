b69e6d0bab4d324c5d41b55a4b384af65ec116f1
Pankaj Deopa <pankajdeopa3.pd@gmail.com>
2020-08-14 11:12:44 +0530

    changed to functional component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,30 +1,22 @@
-import React , { Component } from 'react';
+import React from 'react';
 import './Home.css';
 
-class Home extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-
-		}
-	}
-	render() {
+const Home = ({onRouteChange, user, clearUser}) => {
 		return (
 			<div className = 'Backgroundlayout'>
 			<div className = 'Homenav'>
-			<div className = 'Navitem'>{`₿ ${this.props.user.cash}`}</div>
-			<div className = 'Navitem'>{`Hi, ${this.props.user.username}`}</div>
-			<div className = 'Navitem'>{`${this.props.user.rank}`}</div>
+			<div className = 'Navitem'>{`₿ ${user.cash}`}</div>
+			<div className = 'Navitem'>{`Hi, ${user.username}`}</div>
+			<div className = 'Navitem'>{`${user.rank}`}</div>
 			</div>
 			<div className = 'Buttonbox'>	 
-			    <input className = 'options'  type = 'submit' value = 'Play' onClick = {() => this.props.onRouteChange('play')} />	 
-	 		    <input className = 'options' type = 'submit' value = 'Profile' onClick = {() => this.props.onRouteChange('profile')} />
-			    <input className = 'options' type = 'submit' value = 'Best Gamblers' onClick = {() => {this.props.onRouteChange('serverpos')}} />	 		     
-			    <input className = 'options' type = 'submit' value = 'signout' onClick = {() => {this.props.clearUser(); this.props.onRouteChange('login')}} />
+			    <input className = 'options'  type = 'submit' value = 'Play' onClick = {() => onRouteChange('play')} />	 
+	 		    <input className = 'options' type = 'submit' value = 'Profile' onClick = {() => onRouteChange('profile')} />
+			    <input className = 'options' type = 'submit' value = 'Best Gamblers' onClick = {() => {onRouteChange('serverpos')}} />	 		     
+			    <input className = 'options' type = 'submit' value = 'signout' onClick = {() => {clearUser(); onRouteChange('login')}} />
 			</div>
 			</div>
 		);
-	}
 }
 
 export default Home;
